42787a1ac52f4d43f2582ea206b733d2a3883fdb
Misaki ⋆౨ৎ˚⟡˖ ࣪ <117863598+kripo16@users.noreply.github.com>
2024-08-04 19:51:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const cookies = `cookies`;
+const cookies = `.netflix.com	TRUE	/	FALSE	1751725862	OptanonAlertBoxClosed	2024-07-05T14:31:02.831Z
+.netflix.com	TRUE	/	FALSE	1751726628	nfvdid	BQFmAAEBEND3j1pTjsJ9f0LxOkSG1mVgaJMsWwiLNAmtMhNM4KhLH50tI-9hhAaxBY3ZUh8rMpsfbLeEoDKdydPoO0SBaLj6vPdYUwScFibqTmcafcdcd3uumEw0voxhqobLCKtgTgB9MWz4pOYHlxRkHLTcit7d
+.netflix.com	TRUE	/	FALSE	1752242654	OptanonConsent	isGpcEnabled=0&datestamp=Thu+Jul+11+2024+11%3A04%3A14+GMT-0300+(Hor%C3%A1rio+Padr%C3%A3o+de+Bras%C3%ADlia)&version=202406.1.0&browserGpcFlag=0&isIABGlobal=false&hosts=&consentId=20aadaed-a50f-47fd-a85d-8cd64cc44c23&interactionCount=1&isAnonUser=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0002%3A1%2CC0003%3A1%2CC0004%3A1&intType=3&geolocation=BR%3BSP&AwaitingReconsent=false
+.netflix.com	TRUE	/	FALSE	1728482654	netflix-sans-bold-3-loaded	true
+.netflix.com	TRUE	/	FALSE	1728482654	netflix-sans-normal-3-loaded	true
+.netflix.com	TRUE	/	TRUE	1752335322	NetflixId	ct%3DBgjHlOvcAxL5Aun_XCnRb1qA5gDLXKrSZ2Eb6FqeG8UIRAnkDV08GxvzdffREB-9UoQpdSjqp2WifQOfxKnn4IiNRxT7_ITPWYBDtJVbz2CtcgkpE7qsSd-0e4nVjr8sSvvf4Ni5OqMzSLRXaGfiIfUvgvPxgEGuEwuSbEi1drSHt4y75ZchEQUI4I0x1QqM0OCurX3ZzlKGFUXlkFns5zy8LUYW4sNFtwKdym5Ak4mYzKsnWwb_euor89Q7ksA3yWZbs__NVkCYDo-wD6QZt0fiQLh6w1ZVvJlU1T_U86iqkk6rvBMl2gz4afa9PPPqVr13Mrx-TxgW9ec37o7ythL3b4rfnKK9U-X3vFTfDUjYcJNtO_3jJZMMssyG5-V31MZ2fgaz70dsE3ruJMvFpRi2jtR8izDtzjMe6J4EMHPjUDL3_xyA3oK4u4getu9CfsK6Ry2R96C201sXCwyloPyEOh83EHkwSKsLVgg_q5bR97ndGD0QTK8plY9q80Ax8g9YGAYiDgoMGbbZtEm6De72cPQ2%26ch%3DAQEAEAABABTAGpIR_289VSR_lX6tVvYWUpsvj9040lY.%26v%3D3
+.netflix.com	TRUE	/	TRUE	1752335322	SecureNetflixId	v%3D3%26mac%3DAQEAEQABABSZROm5gdmkR-dc6lJqUM9eo-3Oz2FDqq0.%26dt%3D1720799322905
+`;
 
 document.getElementById('copyCookies').addEventListener('click', function() {
     navigator.clipboard.writeText(cookies)
